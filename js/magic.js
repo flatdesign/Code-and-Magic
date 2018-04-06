fc73e4346e7288d19ffe150425b1ec2a810a39e9
flatdesign <danil.krosis@gmail.com>
2018-04-06 16:49:26 +0300

    Сделал задание 1ой лекции (Знакомство)

---
+++ js/magic.js
@@ -0,0 +1,24 @@
+var fireballSize = 22;
+var wizardSpeed = 3;
+var wizardWidth = 70;
+
+var getFireballSpeed = function(left) {
+	var speed;
+	if (left) {
+		return speed = 5;
+	}
+	return speed = 2;
+}
+
+var getWizardHeight = function() {
+	return 1.337 * wizardWidth;
+}
+
+var getWizardX = function(width) {
+	return width/2;
+}
+
+var getWizardY = function(height) {
+	return height * (1/3);
+}
+
